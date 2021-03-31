55d0549d5ce6f52e4f47651601ff09957d61c8dc
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-03-31 17:42:35 +0900

    3.31수업

--- .idea/수업/3.31class(3).py
+++ .idea/수업/3.31class(3).py
@@ -4,11 +4,16 @@ import turtle as t #터틀그래픽 선언
 
 t.shape('turtle')#터틀그래픽 shape선언
 
-for i in range(-360,360):
+
+
+def moveUp():
+    t.setheading(90)
+    t.forward(30)
+
+t.onkeypress(moveUp,"Up") #위쪽 방향키 입력시 왼쪽 90도 바라보고 30만큼 전진합니다.
 
 
 
 
-
-
+t.listen() #키보드 입력 이벤트 처리를 위하여 필요합니다.
 t.done() #종료
---
+++ .idea/수업/3.31class(4).py
@@ -0,0 +1,13 @@
+import turtle as t
+import math
+t.shape('turtle')
+
+t.penup()
+t.goto(-360,0)
+t.pendown()
+
+for i in range(-360,360):
+    y =math.sin(math.radians(x))*100
+    t.goto(x,y)
+
+t.done()
